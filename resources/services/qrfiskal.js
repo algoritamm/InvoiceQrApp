8e68e5459498056328cb31f268aa8842a57773a1
algoritamm <janajerinic@gmail.com>
2025-03-08 20:18:03 +0100

    Update qrfiskal.js

--- resources/services/qrfiskal.js
+++ resources/services/qrfiskal.js
@@ -1,27 +1,28 @@
-///Form element
+var filename;
+/// Form elements
 const formGetInvoiceData = document.getElementById('getInvoceDataForm');
+const inputUrl = document.getElementById('urlInput');
+const loader = document.getElementById('loader');
+const btnUrlInput = document.getElementById('urlInputBtn');
 
-if(!formGetInvoiceData) {
+if(!formGetInvoiceData || !inputUrl) {
     Neutralino.debug.log("Form element doesn't exists.", "WARNING");
     alert("Error loading form resources...");
 }   
 
-///Toast classification
+/// Toast classification
 const toastBackground = {
     success: "linear-gradient(to right, #00b09b, #96c93d)",
     error: "linear-gradient(to right, #ff5f6d, #ffc371)",
     warning: "linear-gradient(to right,rgb(255, 204, 95),rgb(252, 242, 100))"
 };
-
-///Loader
-function showLoader() {
-    document.getElementById('loader').removeAttribute('hidden');
-}
-function hideLoader() {
-    document.getElementById('loader').setAttribute('hidden', '');
-}
-
-///URL validation
+/// Loader
+const showLoader = () => loader.removeAttribute('hidden');
+const hideLoader = () => loader.setAttribute('hidden', '');
+/// Button
+const showButton = () => btnUrlInput.removeAttribute('hidden');
+const hideButton = () => btnUrlInput.setAttribute('hidden', '');
+/// URL validation
 function isValidURL(url) {
     try {
         new URL(url);
@@ -37,7 +38,7 @@ function isValidURL(url) {
     }
 }
 
-///HTTP GET request
+/// HTTP GET request
 async function sendRequestAndGetDetails(url) {
     const command = `curl -H "Accept: application/json" -H "Content-Type: application/json; charset=utf-8" "${url}"`;
     try {
@@ -52,6 +53,7 @@ async function sendRequestAndGetDetails(url) {
         const data = JSON.parse(response.stdOut);
         Neutralino.debug.log(`Http response: ${data}`);
         return data;
+
     } catch (error) {
         console.error(`Error while loading data: ${error.message}`);
         Neutralino.debug.log(`${error.message}`, "ERROR");
@@ -59,7 +61,7 @@ async function sendRequestAndGetDetails(url) {
     }
 }
 
-///Load data from response
+/// Load data from response
 async function createUpdateExcelFile(jsonData) {
     if (!jsonData) throw new Error("Data isn't valid.");
     
@@ -77,11 +79,13 @@ async function createUpdateExcelFile(jsonData) {
     
         const date = new Date();
         const formattedDate = `${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, '0')}${String(date.getDate()).padStart(2, '0')}`;
-        const filename = `InvoceData_${formattedDate}.xlsx`;
+        filename = `InvoceData_${formattedDate}.xlsx`;
         const path = await Neutralino.os.getPath('downloads');
         const filePath = `${path}/${filename}`;
+
         console.log(filePath)
         Neutralino.debug.log(`Download file path: ${filePath}`);
+
         let wb, ws;
         const fileExists = await Neutralino.filesystem.readDirectory(path) 
             .then(files => files.some(file => file.entry === filename))
@@ -94,26 +98,34 @@ async function createUpdateExcelFile(jsonData) {
             ws = workbook.Sheets[workbook.SheetNames[0]];
             XLSX.utils.sheet_add_json(ws, [dataRow], { skipHeader: true, origin: -1 });
             wb = workbook;    
+
         } else {
             //if file doesn't exist create new
             ws = XLSX.utils.json_to_sheet([dataRow]);
             wb = XLSX.utils.book_new();
             XLSX.utils.book_append_sheet(wb, ws, "Sheet");
         }
+
         //console.log(wb)
         const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
         await Neutralino.filesystem.writeBinaryFile(filePath, wbout);
-        
-        let pathParts = await Neutralino.filesystem.getPathParts(filePath);
-        return pathParts.relativePath;
+        return {Success: true, File: filePath};
 
     } catch(error) {
-        Neutralino.debug.log(`${error.message}`, "ERROR");
-        throw new Error(`Error:| ${error}.`);
+        if (error.code === 'EPERM' || error.code === 'EACCES' || error.code === 'NE_FS_FILWRER')
+        {
+            Neutralino.debug.log(`File is locked or arleady oppened : ${error.message}`, "ERROR");
+            console.log("ERROR", error.code, error.message);
+            return {Success: false, File: filename}
+        }
+
+        Neutralino.debug.log(`Unespected error : ${error.message}`, "ERROR");
+        console.log("ERROR", error.code, error.message);
+        throw new Error(`Error: | ${error.message}.`);
     }      
 }
 
-///Create toast alert notification
+/// Create toast alert notification
 function showToast(message, type) {
     Toastify({
         text: message,
@@ -123,12 +135,9 @@ function showToast(message, type) {
     }).showToast();
 }
 
-///Main function
-formGetInvoiceData.addEventListener('submit', async function (event) {
-    event.preventDefault();
-    const inputUrl = document.getElementById('urlInput');
-    const url = inputUrl.value.trim();
-
+/// Handle data
+async function handleRequest(url) {
+    console.log(url);
     showLoader();
     try {
         if (!isValidURL(url)) {
@@ -139,13 +148,20 @@ formGetInvoiceData.addEventListener('submit', async function (event) {
         
         const data = await sendRequestAndGetDetails(url);
         if (data) {
-            var createdFile = await createUpdateExcelFile(data);
-            showToast(`Successfully loaded data ${createdFile}.`, "success");
+            var response = await createUpdateExcelFile(data);
+            if(response.Success) {
+                showToast(`Successfully saved data.`, "success");
+                showToast(`File path: ${response.File}.`, "success");
+            }
+            else {
+                showToast(`First close file ${response.File}.`, "warning");
+            }                
         }
         else {
             showToast("Error while loading data.", "error");
             Neutralino.debug.log(`Error while loading data.`, "ERROR");
         }
+
     } catch (error) {
         console.error(error);
         Neutralino.debug.log(`${error.message}`, "ERROR");
@@ -154,4 +170,22 @@ formGetInvoiceData.addEventListener('submit', async function (event) {
         hideLoader();
         formGetInvoiceData.reset();
     }
+}
+
+/// Main function
+/// On value changed or input
+inputUrl.addEventListener("input", async function(){   
+    if(this.value.includes("\n")){
+        hideButton();
+        await handleRequest(this.value.trim());
+    } 
+    else {
+        showButton();
+    }   
+});
+/// On submit form - button click submit
+formGetInvoiceData.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    hideButton();
+    handleRequest(inputUrl.value.trim());
 });
